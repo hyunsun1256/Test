d7a16e9475a2aa4eb204990209a3d6067cec9033
JBEDU06 <phs1256@daum.net>
2023-11-20 11:20:35 +0900

    frgs

---
+++ src/Test05.java
@@ -0,0 +1,25 @@
+
+public class Test05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+int a=10, b=20;
+int result1;
+
+result1 =a+b;
+
+System.out.println(result1);
+
+
+Steing strl= "100";
+
+int intStr = Integer.p strl;
+
+String str3 = String.valueOf(inSt)
+
+	}
+	
+
+}
---
+++ src/test01/Test_jaba.java
@@ -0,0 +1,24 @@
+package test01;
+
+public class Test_jaba {
+
+	public static void main(String[] args) {  //메인 매소드
+		// TODO Auto-generated method stub
+//a=10
+//print(a)
+		
+		int a = 10;
+		double b = 10.5;
+		int c;
+			c= 100;
+		
+		int result =a + c;
+		System.out.println(result);
+		
+		int value;
+		//int res =value + 10; 초기화 안 되어 에러 발생
+		
+		System.out.println("c의 값은 "+ c + "이빈다.");	
+	}
+
+}
